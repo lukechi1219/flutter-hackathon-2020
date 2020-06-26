79b3e53da00a9fb3ca4f08826e0a0efdcc2430f0
TANGCHENGLEE <virgil246@gmail.com>
2020-06-27 00:11:16 +0800

    wip

--- lib/ui/page/Home.dart
+++ lib/ui/page/Home.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+
 class Home extends StatelessWidget {
+    Location myUserLocation;
+
+  GoogleMapController mapController;
+//TODO:Define Center With GPS
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+Marker marker =Marker()
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("HOME"),
-      
-      
+      child: GoogleMap(
+        markers: ,
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(target: _center)),
+          
     );
   }
 }
--- lib/ui/page/Notification.dart
+++ lib/ui/page/Notification.dart
@@ -1,9 +1,39 @@
 import 'package:flutter/material.dart';
+
 class Notify extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("NOTI"),
-      
+    return Center(
+        child: ListView(
+          //TODO: Get Notify Message
+      children: <Widget>[
+        NotifyBuild(icon: Icons.mail, title: "title", content: "content")
+      ],
+    ));
+  }
+}
+
+class NotifyBuild extends StatelessWidget {
+  const NotifyBuild({
+    Key key,
+    @required this.icon,
+    @required this.title,
+    @required this.content,
+  }) : super(key: key);
+
+  final IconData icon;
+  final String title;
+  final String content;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(icon),
+        title: Text(title),
+        subtitle: Text(content),
+      ),
     );
   }
 }
