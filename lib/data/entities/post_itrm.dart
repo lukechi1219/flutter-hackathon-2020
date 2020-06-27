6d6d0b60a3384b3bc1f3b32f3a37fd2cd6852654
Luke Chi <luke.chi@gmail.com>
2020-06-27 10:40:11 +0800

    add entity PostItem

---
+++ lib/data/entities/post_itrm.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:meta/meta.dart';
+
+class PostItem extends Equatable {
+  final String text;
+  final LatLng location;
+  final String creator;
+  final DateTime createTime;
+  // 截止時間
+  final DateTime postEndTime;
+
+  PostItem({
+    @required this.text,
+    @required this.location,
+    @required this.creator,
+    @required this.createTime,
+    // 如果是接單 才有 截止時間. 純發佈消息不一定要有截止時間
+    this.postEndTime,
+  });
+
+  @override
+  List<Object> get props => [
+        text,
+        location.toString(),
+        creator,
+        createTime.millisecond,
+        (postEndTime == null) ? '' : postEndTime.millisecond,
+      ];
+}
