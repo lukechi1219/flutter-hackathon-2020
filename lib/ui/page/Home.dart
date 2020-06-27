ccfd3a7631d2e05216e2bf597f38b6d9ae3568fa
Luke Chi <luke.chi@gmail.com>
2020-06-27 16:28:12 +0800

    getNearByPostItems 的範例程式

--- lib/ui/page/Home.dart
+++ lib/ui/page/Home.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutterhood/core/usecases/usecase.dart';
+import 'package:flutterhood/domain/usecases/get_near_by_post_items.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
+
+import '../../injection_container.dart';
 
 class Home extends StatelessWidget {
   Location myUserLocation;
@@ -11,11 +15,34 @@ class Home extends StatelessWidget {
 //  final LatLng _center = const LatLng(45.521563, -122.677433);
   // Taipei 101
   final LatLng _center = const LatLng(25.0326811, 121.5646961);
+
   // Google Office NYC Chelsea
 //  final LatLng _center = const LatLng(40.7420835, -74.0061156);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+
+    getPostItems();
+  }
+
+  void getPostItems() async {
+    //
+    GetNearByPostItems getNearByPostItems = getIt.get<GetNearByPostItems>();
+
+    final failureOrPostItems = await getNearByPostItems(NoParams());
+
+    failureOrPostItems.fold(
+      (failure) => print(failure),
+      (items) {
+        print('------');
+        for (var postItem in items) {
+          var latLng = postItem.location;
+          print(
+              '${postItem.text}: ${postItem.creator}, time: ${postItem.createTime}, location: (${latLng.latitude}, ${latLng.longitude})');
+        }
+        print('------');
+      },
+    );
   }
 
 //  Marker marker = Marker();
@@ -48,12 +75,13 @@ class Home extends StatelessWidget {
       markerId: MarkerId('testMarker' + DateTime.now().millisecond.toString()),
       position: _center,
       infoWindow: InfoWindow(
-        onTap: (){print("object");},
+        onTap: () {
+          print("object");
+        },
         title: 'Really cool place',
         snippet: '5 Star Rating',
       ),
       icon: BitmapDescriptor.defaultMarker,
-      
     ));
   }
 }
