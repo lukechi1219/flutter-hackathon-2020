dd8c99ab7eb073b71995a3d05187031a2f118759
Luke Chi <luke.chi@gmail.com>
2020-06-27 09:57:34 +0800

    Update Home.dart

--- lib/ui/page/Home.dart
+++ lib/ui/page/Home.dart
@@ -3,24 +3,53 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
 class Home extends StatelessWidget {
-    Location myUserLocation;
+  Location myUserLocation;
 
   GoogleMapController mapController;
+
 //TODO:Define Center With GPS
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+//  final LatLng _center = const LatLng(45.521563, -122.677433);
+  // Taipei 101
+  final LatLng _center = const LatLng(25.0326811, 121.5646961);
+  // Google Office NYC Chelsea
+//  final LatLng _center = const LatLng(40.7420835, -74.0061156);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
-Marker marker =Marker()
+
+//  Marker marker = Marker();
+  final Set<Marker> _markers = {};
+
   @override
   Widget build(BuildContext context) {
+    //
+    _addTestMarker();
+
     return Center(
       child: GoogleMap(
-        markers: ,
+          markers: _markers,
           onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(target: _center)),
-          
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 15,
+          )),
     );
+  }
+
+  /*
+   * test
+   */
+  void _addTestMarker() {
+    _markers.add(Marker(
+      // This marker id can be anything that uniquely identifies each marker.
+      markerId: MarkerId('testMarker' + DateTime.now().millisecond.toString()),
+      position: _center,
+      infoWindow: InfoWindow(
+        title: 'Really cool place',
+        snippet: '5 Star Rating',
+      ),
+      icon: BitmapDescriptor.defaultMarker,
+    ));
   }
 }
