7774f1276f6780c8f8c197c6580b262ceae7a964
TANGCHENGLEE <virgil246@gmail.com>
2020-06-28 13:22:49 +0800

    Add assets

--- lib/ui/page/Landing.dart
+++ lib/ui/page/Landing.dart
@@ -1,1 +1,16 @@
-.
+import 'package:flutter/material.dart';
+
+class Landing extends StatefulWidget {
+  @override
+  _LandingState createState() => _LandingState();
+}
+
+class _LandingState extends State<Landing> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      
+    );
+  }
+}
