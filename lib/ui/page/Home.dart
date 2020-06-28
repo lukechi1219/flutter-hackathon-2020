a4360868981a66bb6369c566b708da7db70950a5
Luke Chi <luke.chi@gmail.com>
2020-06-28 16:40:31 +0800

    Update Home.dart

--- lib/ui/page/Home.dart
+++ lib/ui/page/Home.dart
@@ -239,7 +239,7 @@ class _HomeState extends State<Home> {
    */
   void updatePinOnMap() async {
     CameraPosition cPosition = CameraPosition(
-      zoom: 15,
+      zoom: 12,
       target: _center,
     );
 
